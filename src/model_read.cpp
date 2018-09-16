e3373e0b234349ee33454f5f146cc6a78e719757
Wheest <perry@gibsonic.org>
2018-09-16 14:03:22 +0100

    my first commit

---
+++ src/model_read.cpp
@@ -0,0 +1,19 @@
+#include "flatbuffers/flatbuffers.h"
+#include "schema_generated.h"
+#include <iostream> // C++ header file for printing
+#include <fstream> // C++ header file for file access
+
+
+int main(int argc, char *argv[])
+{
+  std::ifstream infile;
+  infile.open("monsterdata_test.mon", std::ios::binary | std::ios::in);
+  infile.seekg(0,std::ios::end);
+  int length = infile.tellg();
+  infile.seekg(0,std::ios::beg);
+  char *data = new char[length];
+  infile.read(data, length);
+  infile.close();
+
+  return 0;
+}
