8f4262584122cfa0da80d1df7c4c63e671daa74e
Wheest <perry@gibsonic.org>
2018-09-16 14:13:43 +0100

    minor alter

--- src/model_read.cpp
+++ src/model_read.cpp
@@ -6,14 +6,14 @@
 
 int main(int argc, char *argv[])
 {
-  std::ifstream infile;
-  infile.open("monsterdata_test.mon", std::ios::binary | std::ios::in);
-  infile.seekg(0,std::ios::end);
-  int length = infile.tellg();
-  infile.seekg(0,std::ios::beg);
-  char *data = new char[length];
-  infile.read(data, length);
-  infile.close();
+  // std::ifstream infile;
+  // infile.open("monsterdata_test.mon", std::ios::binary | std::ios::in);
+  // infile.seekg(0,std::ios::end);
+  // int length = infile.tellg();
+  // infile.seekg(0,std::ios::beg);
+  // char *data = new char[length];
+  // infile.read(data, length);
+  // infile.close();
 
   return 0;
 }
